1ae1cd24402597b3400ead55fe60aedf37de28a7
endriu17 <aolaszewski@gmail.com>
2018-08-01 01:20:33 +0200

    add clone function to between links and general sides

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-function sidebarHide(){
+function sidebarHide() {
   document.getElementById("sidebar_hide").style.display = "none";
   document.getElementById("hamburger-menu__hiden").style.display = "block";
   document.getElementById("content-margin").style.margin = "50px 0 0 0";
 
 }
 
-function sidebarOpen(){
+function sidebarOpen() {
   document.getElementById("sidebar_hide").style.display = "block";
   document.getElementById("hamburger-menu__hiden").style.display = "none";
   document.getElementById("content-margin").style.margin = "50px 0 0 200px";
 }
+
+
+  var linksContent = document.querySelector('link[rel="import"]');
+
+  var generalContent = linksContent.import.querySelector('#content-links_export');
+  var clone = document.importNode(generalContent, true);
+  
+  document.querySelector('#content-links_import').appendChild(clone);
+  
