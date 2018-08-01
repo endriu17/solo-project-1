7540763ac187bfc0e629feab85e46bed67163fe5
endriu17 <aolaszewski@gmail.com>
2018-08-02 00:00:51 +0200

    add Quit modal

--- js/script.js
+++ js/script.js
@@ -18,3 +18,48 @@ var generalContent = linksContent.import.querySelector('#content-links_export');
 var clone = document.importNode(generalContent, true);
 
 document.querySelector('#content-links_import').appendChild(clone);
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show')
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault()
+    closeModal()
+  })
+})
+
+document.querySelectorAll('#overlay .js--cancel-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault()
+    closeModal()
+  })
+})
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal()
+  }
+})
+
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal()
+  }
+})
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show')
+  })
+  document.querySelector('#overlay').classList.add('show')
+  document.querySelector(modal).classList.add('show')
+}
+
+// var quitModal = document.getElementById('quitModal');
+
+// function openModal(modal) {
+//   document.getElementById("overlay").style.display = "flex";
+//   document.getElementById("quitModal").style.display = "block";
+// }
