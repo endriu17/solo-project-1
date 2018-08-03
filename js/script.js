5822846b679a4f410c46f145d62c0269d92f0edc
endriu17 <aolaszewski@gmail.com>
2018-08-03 13:04:56 +0200

    add dynamic chart to general side content

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// sidebar show hidde
+
 function sidebarHide() {
   document.getElementById("sidebar_hide").style.display = "none";
   document.getElementById("hamburger-menu__hiden").style.display = "block";
@@ -11,6 +13,56 @@ function sidebarOpen() {
   document.getElementById("content-margin").style.margin = "50px 0 0 200px";
 }
 
+// chart
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  // 1
+  type: 'bar',
+  data: {
+      // 2
+      labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+      // 3
+      datasets: [{
+          // 4
+          label: "Signups",
+          // 5
+          backgroundColor: '#8DBEC8',
+          borderColor: '#8DBEC8',
+          // 6
+          data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+      },
+      {
+          label: "FTD",
+          backgroundColor: '#F29E4E',
+          borderColor: '#F29E4E',
+          data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+      },
+      {
+          label: "Earned",
+          backgroundColor: '#71B374',
+          borderColor: '#71B374',
+          data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+          // 7
+          hidden: true,
+      }]
+  },
+  options: {
+    legend: {
+      display: true,
+      labels: {
+      padding: 30,
+      boxWidth: 20,
+      radius: 10,
+      usePointStyle: true,
+      pointStyle: 5,
+      }
+    }
+  }
+});
+
+// links import to general
 
 var linksContent = document.querySelector('link[rel="import"]');
 
@@ -18,6 +70,8 @@ var generalContent = linksContent.import.querySelector('#content-links_export');
 var clone = document.importNode(generalContent, true);
 
 document.querySelector('#content-links_import').appendChild(clone);
+
+// modal functions
 
 function closeModal() {
   document.getElementById('overlay').classList.remove('show')
